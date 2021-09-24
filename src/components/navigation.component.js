cf24d41b8e537de3a938455638e900eacb5b6e7b
Radik Abubjazov <abubjazov@mail.ru>
2021-09-24 19:01:56 +0300

    + навигация по Табам

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -3,10 +3,15 @@ import { Component } from '../core/component'
 export class NavigationComponent extends Component {
     constructor(id) {
         super(id)
+        this.tabs = []
     }
 
     init() {
        this.$elem.addEventListener('click', tabClickHandler.bind(this))
+    }
+
+    registerTabs(tabs) {
+        this.tabs = tabs
     }
 }
 
@@ -14,7 +19,12 @@ function tabClickHandler(event) {
     event.preventDefault()
     if (event.target.classList.contains('tab')) {
         this.$elem.querySelectorAll('.tab').forEach(e => e.classList.remove('active'))
+        this.tabs.forEach(t => t.component.hide())
 
         event.target.classList.add('active')
+        this.tabs.find(t => t.name === event.target.dataset.name).component.show()
+        
+
+
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import './index.css'
 import { HeaderComponent } from './components/header.component'
 import { NavigationComponent } from './components/navigation.component'
+import { CreateComponent } from './components/create.component'
+import { PostsComponent } from './components/posts.component'
+import { FavoriteComponent } from './components/favorite.component'
 
 new HeaderComponent('header')
 
 const navigation = new NavigationComponent('navigation')
+// new CreateComponent('create')
+// new PostsComponent('posts')
+// new FavoriteComponent('favorite')
+
+navigation.registerTabs([
+    {name: 'create', component: new CreateComponent('create')},
+    {name: 'posts', component: new PostsComponent('posts')},
+    {name: 'favorite', component: new FavoriteComponent('favorite')}
+])
