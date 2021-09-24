dff5703d788a01120e290ec3c53cd83562d57652
Radik Abubjazov <abubjazov@mail.ru>
2021-09-24 19:37:16 +0300

    + собираем данные с формы

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,7 +1,27 @@
 import { Component } from '../core/component'
+import { Form } from '../core/form'
 
 export class CreateComponent extends Component {
     constructor(id) {
         super(id)
     }
+
+    init() {
+        this.$elem.addEventListener('submit', submitHandler.bind(this))
+        this.form = new Form(this.$elem, {
+            title: [],
+            fulltext: []
+        })
+    }
+}
+
+function submitHandler(event) {
+    event.preventDefault()
+
+    const formData = {
+        type: this.$elem.type.value,
+        ...this.form.value()
+    }
+
+    console.log('Form Submitted!', formData)
 }
