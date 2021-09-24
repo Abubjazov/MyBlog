53d9206365da7f152db6e98341e3febfbde7f631
Radik Abubjazov <abubjazov@mail.ru>
2021-09-24 23:52:32 +0300

    + вывод постов на страницу

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -9,8 +9,34 @@ export class PostsComponent extends Component {
 
     async onShow() {
         const fbData = await apiService.getPosts(),
-              posts = ParseService.fbObjToArray(fbData)
+              posts = ParseService.fbObjToArray(fbData),
+              html = posts.map(post => renderPost(post)).join(' ')
 
-        console.log(posts)
+        this.$elem.innerHTML = html
     }
+}
+
+function renderPost(post) {
+    const tag = post.type === 'news' 
+        ? '<li class="tag tag-blue tag-rounded">Новость</li>' 
+        : '<li class="tag tag-rounded">Заметка</li>'
+
+    const button = '<button class="button-round button-small button-primary">В избранное</button>'
+    return `
+        <div class="panel">
+        <div class="panel-head">
+            <p class="panel-title">${post.title}</p>
+            <ul class="tags">
+            ${tag}
+            </ul>
+        </div>
+        <div class="panel-body">
+            <p class="multi-line">${post.fulltext}</p>
+        </div>
+        <div class="panel-footer w-panel-footer">
+            <small>${post.date} ${post.time}</small>
+            ${button}
+        </div>
+        </div>
+    `
 }
--- src/services/parse.service.js
+++ src/services/parse.service.js
@@ -1,11 +1,11 @@
 export class ParseService {
     static fbObjToArray(fbData) {
-        
-        return Object.keys(fbData).map(key => {
+
+        return (Object.keys(fbData).map(key => {
             const item = fbData[key]
             item.id = key
             
             return item
-        })
+        })).reverse()
     }
 }
